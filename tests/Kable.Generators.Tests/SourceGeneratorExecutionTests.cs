afbc2bcdec084c578df0cafbb76a18a6470c2df4
jaon13 <7531365+jaon13@users.noreply.github.com>
2026-09-04 15:14:19 +0900

    test: implement TC-GEN-101 (Roslyn incremental generator driver execution and diagnostics)

--- tests/Kable.Generators.Tests/SourceGeneratorExecutionTests.cs
+++ tests/Kable.Generators.Tests/SourceGeneratorExecutionTests.cs
@@ -1,7 +1,14 @@
 namespace Kable.Generators.Tests;
 
+using System;
+using System.Collections.Immutable;
+using System.IO;
+using System.Linq;
+using System.Reflection;
 using FluentAssertions;
 using Kable.Generators;
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CSharp;
 using Xunit;
 
 [DeviceCommand("oPON")]
@@ -55,5 +62,45 @@ public class SourceGeneratorExecutionTests
         var cmd = new TestValveCommand("V12", 1);
         cmd.FormatWireMessage().Should().Be("oVALVE.V12:1");
         cmd.IsUrgent.Should().BeFalse();
+    }
+
+    [Fact]
+    public void TC_GEN_101_Generator_DriverExecution_GeneratesExpectedSourcesWithoutDiagnostics()
+    {
+        string userSource = @"
+namespace TestApp;
+using Kable.Generators;
+
+[DeviceCommand(""SAMPLE.{Id}:{Volume}"")]
+public readonly partial record struct MeasureSampleCommand(string Id, double Volume);
+";
+
+        var syntaxTree = CSharpSyntaxTree.ParseText(userSource);
+        var references = new[]
+        {
+            MetadataReference.CreateFromFile(typeof(object).Assembly.Location),
+            MetadataReference.CreateFromFile(Assembly.Load("System.Runtime").Location)
+        };
+
+        var compilation = CSharpCompilation.Create("TestCompilation",
+            new[] { syntaxTree },
+            references,
+            new CSharpCompilationOptions(OutputKind.DynamicallyLinkedLibrary));
+
+        var generator = new ProtocolSourceGenerator();
+        GeneratorDriver driver = CSharpGeneratorDriver.Create(generator);
+
+        driver = driver.RunGeneratorsAndUpdateCompilation(compilation, out var outputCompilation, out var diagnostics);
+
+        diagnostics.Should().BeEmpty();
+        var runResult = driver.GetRunResult();
+        runResult.GeneratedTrees.Length.Should().BeGreaterThanOrEqualTo(2); // KableAttributes.g.cs + MeasureSampleCommand.g.cs
+
+        var generatedCode = runResult.GeneratedTrees
+            .Select(t => t.ToString())
+            .FirstOrDefault(s => s.Contains("MeasureSampleCommand"));
+
+        generatedCode.Should().NotBeNull();
+        generatedCode.Should().Contain("$\"SAMPLE.{Id}:{Volume}\"");
     }
 }
